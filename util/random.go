07b631edb20b6cd6e5e66eada5707223a13b7311
Roberto Calestini <betocalestini@gmail.com>
2022-09-26 18:05:34 -0300

    refactory: creating map of amostra

--- util/random.go
+++ util/random.go
@@ -7,15 +7,43 @@ import (
 	"time"
 )
 
-const lettersWithSimbols = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ/;.,~][!@#$%*()_+}{:?><|1234567890"
-const alphabet = "abcdefghijklmnopqrstuvwxyz"
-const alphabetWithCapital = "abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-const numbers = "0123456789"
-const numbersLessZero = "123456789"
-const binaries = "01"
-const zeroTwo = "012"
-const zeroThree = "0123"
-const zeroSix = "0123456"
+//trabalhando com struct
+// type typeOfAmostra struct {
+// 	lettersWithSimbols  string
+// 	alphabet            string
+// 	alphabetWithCapital string
+// 	numbers             string
+// 	numbersLessZero     string
+// 	binaries            string
+// 	zeroTwo             string
+// 	zeroThree           string
+// 	zeroSix             string
+// }
+
+// var a = typeOfAmostra{"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ/;.,~][!@#$%*()_+}{:?><|1234567890", "abcdefghijklmnopqrstuvwxyz", "abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ", "0123456789", "123456789", "01", "012", "0123", "0123456"}
+
+// const lettersWithSimbols = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ/;.,~][!@#$%*()_+}{:?><|1234567890"
+// const alphabet = "abcdefghijklmnopqrstuvwxyz"
+// const alphabetWithCapital = "abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+// const numbers = "0123456789"
+// const numbersLessZero = "123456789"
+// const binaries = "01"
+// const zeroTwo = "012"
+// const zeroThree = "0123"
+// const zeroSix = "0123456"
+
+// trabalhando com map
+var m = map[string]string{
+	"lettersWithSimbols":  "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ/;.,~][!@#$%*()_+}{:?><|1234567890",
+	"alphabet":            "abcdefghijklmnopqrstuvwxyz",
+	"alphabetWithCapital": "abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+	"numbers":             "0123456789",
+	"numbersLessZero":     "123456789",
+	"binaries":            "01",
+	"zeroTwo":             "012",
+	"zeroThree":           "0123",
+	"zeroSix":             "0123456",
+}
 
 // Gerar strings aleatórias de acordo com o tamanho e amostra passados como parâmetros
 func Random(n int, amostra string) string {
@@ -32,9 +60,9 @@ func Random(n int, amostra string) string {
 // Gerar string aleatória de acordo com o tamanho da string passado como parâmetro
 func RandomString(number int) string {
 	var sb strings.Builder
-	k := len(alphabet)
+	k := len(m["alphabet"])
 	for i := 0; i < number; i++ {
-		c := alphabet[rand.Intn(k)]
+		c := m["alphabet"][rand.Intn(k)]
 		sb.WriteByte(c)
 	}
 	return sb.String()
@@ -42,25 +70,25 @@ func RandomString(number int) string {
 
 // gerar email aleatório
 func RandomEmail() string {
-	email := Random(10, alphabet) + "@" + Random(5, alphabet) + ".com"
+	email := Random(10, m["alphabet"]) + "@" + Random(5, m["alphabet"]) + ".com"
 	return email
 }
 
 // gerar senha aleatória com n dígitos
 func RandomSenha(n int) string {
-	senha := Random(n, lettersWithSimbols)
+	senha := Random(n, m["lettersWithSimbols"])
 	return senha
 }
 
 // gerar nome aleatório com n dígitos
 func RandomName(n int) string {
-	name := Random(n, alphabetWithCapital)
+	name := Random(n, m["alphabetWithCapital"])
 	return name
 }
 
 // gerar número aleatório com n dígitos
 func RandomNumber(n int) int {
-	number, err := strconv.Atoi(Random(n, numbers))
+	number, err := strconv.Atoi(Random(n, m["numbers"]))
 	if err != nil {
 		panic(err)
 	}
@@ -71,21 +99,21 @@ func RandomNumber(n int) int {
 func RandomFloatString(n, d int) string {
 	var stringOfNumber string
 	if n > 1 {
-		primeiroDigito := Random(1, numbers)
+		primeiroDigito := Random(1, m["numbers"])
 		if primeiroDigito != "0" {
-			stringOfNumber = primeiroDigito + Random(n-1, numbers) + "." + Random(d, numbers)
+			stringOfNumber = primeiroDigito + Random(n-1, m["numbers"]) + "." + Random(d, m["numbers"])
 		} else {
-			stringOfNumber = Random(n-1, numbers) + "." + Random(d, numbers)
+			stringOfNumber = Random(n-1, m["numbers"]) + "." + Random(d, m["numbers"])
 		}
 	} else {
-		stringOfNumber = Random(n, numbers) + "." + Random(d, numbers)
+		stringOfNumber = Random(n, m["numbers"]) + "." + Random(d, m["numbers"])
 	}
 	return stringOfNumber
 }
 
 // gerar número aletório com n digitos e d casas decimais
 func RandomFloat(n, d int) float64 {
-	stringOfNumber := Random(n, numbers) + "." + Random(d, numbers)
+	stringOfNumber := Random(n, m["numbers"]) + "." + Random(d, m["numbers"])
 	number, err := strconv.ParseFloat(stringOfNumber, 64)
 	if err != nil {
 		panic(err)
@@ -95,7 +123,7 @@ func RandomFloat(n, d int) float64 {
 
 // gerar ano aleatório com 4 dígitos até 2029
 func RandomYear() int {
-	number, err := strconv.Atoi("202" + Random(1, numbers))
+	number, err := strconv.Atoi("202" + Random(1, m["numbers"]))
 	if err != nil {
 		panic(err)
 	}
@@ -104,12 +132,12 @@ func RandomYear() int {
 
 // gerar mês aleatório
 func RandomMonth() int {
-	primeiroDigito := Random(1, binaries)
+	primeiroDigito := Random(1, m["binaries"])
 	var segundoDigito string
 	if primeiroDigito == "0" {
-		segundoDigito = Random(1, numbersLessZero)
+		segundoDigito = Random(1, m["numbersLessZero"])
 	} else {
-		segundoDigito = Random(1, zeroTwo)
+		segundoDigito = Random(1, m["zeroTwo"])
 	}
 	number, err := strconv.Atoi(primeiroDigito + segundoDigito)
 	if err != nil {
@@ -120,15 +148,15 @@ func RandomMonth() int {
 
 // gerar dia aleatório
 func RandomDay() int {
-	primeiroDigito := Random(1, zeroThree)
+	primeiroDigito := Random(1, m["zeroThree"])
 	var segundoDigito string
 	switch {
 	case primeiroDigito == "0":
-		segundoDigito = Random(1, numbersLessZero)
+		segundoDigito = Random(1, m["numbersLessZero"])
 	case primeiroDigito == "3":
-		segundoDigito = Random(1, binaries)
+		segundoDigito = Random(1, m["binaries"])
 	default:
-		segundoDigito = Random(1, numbers)
+		segundoDigito = Random(1, m["numbers"])
 	}
 	number, err := strconv.Atoi(primeiroDigito + segundoDigito)
 	if err != nil {
@@ -139,12 +167,12 @@ func RandomDay() int {
 
 // gerar hora aleatória
 func RandomHour() int {
-	primeiroDigito := Random(1, zeroTwo)
+	primeiroDigito := Random(1, m["zeroTwo"])
 	var segundoDigito string
 	if primeiroDigito == "0" {
-		segundoDigito = Random(1, numbersLessZero)
+		segundoDigito = Random(1, m["numbersLessZero"])
 	} else {
-		segundoDigito = Random(1, zeroTwo)
+		segundoDigito = Random(1, m["zeroTwo"])
 	}
 	number, err := strconv.Atoi(primeiroDigito + segundoDigito)
 	if err != nil {
@@ -155,15 +183,15 @@ func RandomHour() int {
 
 // gerar hora aleatória
 func RandomMinutes() int {
-	primeiroDigito := Random(1, zeroSix)
+	primeiroDigito := Random(1, m["zeroSix"])
 	var segundoDigito string
 	switch {
 	case primeiroDigito == "0":
-		segundoDigito = Random(1, numbersLessZero)
+		segundoDigito = Random(1, m["numbersLessZero"])
 	case primeiroDigito == "6":
 		segundoDigito = "0"
 	default:
-		segundoDigito = Random(1, numbers)
+		segundoDigito = Random(1, m["numbers"])
 	}
 	number, err := strconv.Atoi(primeiroDigito + segundoDigito)
 	if err != nil {
