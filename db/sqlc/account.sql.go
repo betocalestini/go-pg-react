e339f544851603658eaf5415aa6f220186370212
Roberto Calestini <betocalestini@gmail.com>
2022-11-05 14:46:54 -0300

    refactory: change GetAccountsReports return to []uint8

--- db/sqlc/account.sql.go
+++ db/sqlc/account.sql.go
@@ -209,9 +209,9 @@ type GetAccountsReportsParams struct {
 	Type   string `json:"type"`
 }
 
-func (q *Queries) GetAccountsReports(ctx context.Context, arg GetAccountsReportsParams) (int64, error) {
+func (q *Queries) GetAccountsReports(ctx context.Context, arg GetAccountsReportsParams) ([]uint8, error) {
 	row := q.queryRow(ctx, q.getAccountsReportsStmt, getAccountsReports, arg.UserID, arg.Type)
-	var sum_value int64
+	var sum_value []uint8
 	err := row.Scan(&sum_value)
 	return sum_value, err
 }
